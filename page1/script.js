701b8ab4d21dbc313ede6d9914b6dc827f8990cc
ssejinnnn <s7e.song@gmail.com>
2026-02-19 18:30:16 -0500

    Add files via upload

--- page1/script.js
+++ page1/script.js
@@ -599,7 +599,7 @@ if(enterBtn){
 }
 
 /* SOUND */
-const tick = new Audio("/Users/sejin/Documents/Parsons/interaction stu/project1/tick.wav"); // ✅ relative path
+const tick = new Audio("tick.wav"); // ✅ relative path
 tick.loop = true;
 tick.volume = 0.35;
 
